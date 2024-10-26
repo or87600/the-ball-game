2e3c0aa264c56000a07269d0405cb2654b5f2f59
Or Antebi <or87600@gmail.com>
2024-10-27 00:30:09 +0300

    Add getRandomColor function and change ball color

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function onBallClick(elBall) {
     const currWidth = parseInt(window.getComputedStyle(elBall).width)
     const randSize = getRandomIntInclusive(20, 50)
     var newDiameter = currWidth + randSize
+    const randColor = getRandomColor()
     
     if (newDiameter > 400) {
         newDiameter = 100
@@ -11,5 +12,6 @@ function onBallClick(elBall) {
     
     elBall.style.width = newDiameter + 'px'
     elBall.style.height = newDiameter + 'px'
+    elBall.style.backgroundColor = randColor
     elBall.innerText = newDiameter
 }

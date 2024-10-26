baa6821f9f665aa135882551e162cb964139b7b2
Or Antebi <or87600@gmail.com>
2024-10-26 20:26:36 +0300

    Add ball logic to grow each click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 'use strict'
 
+function onBallClick(elBall) {
+    const currWidth = parseInt(window.getComputedStyle(elBall).width)
+    const newDiameter = currWidth + 50
 
-function onBallClick() {
-    console.log('Ball is clicked.')
-
+    elBall.style.width = newDiameter + 'px'
+    elBall.style.height = newDiameter + 'px'
+    elBall.innerText = newDiameter
 }

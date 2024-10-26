945ea245dac5b0116391af766c81372816a671da
Or Antebi <or87600@gmail.com>
2024-10-27 00:22:51 +0300

    Change file name

--- js/unil.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min)
-    const maxFloored = Math.floor(max)
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
-}
